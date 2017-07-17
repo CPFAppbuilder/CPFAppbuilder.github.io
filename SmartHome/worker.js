3a2f3de9b190dbf853782cb683fce2e65ea3c4b9
CPFAppbuilder <ziyousong1221@gmail.com>
2017-07-17 10:33:49 +0800

    Add files via upload

--- SmartHome/worker.js
+++ SmartHome/worker.js
@@ -1,6 +1,6 @@
 onmessage = function(e) {
 	
-	//setup();
+	setup();
 	function setup(){
 		if(cpf)
 			var ret = cpf.setPinMode('["resetPin"],["setPinMode", "analog", 0, "INPUT"],["setPinMode", "analog", 1, "INPUT"],["setPinMode", "analog", 2, "INPUT"],["setPinMode", "analog", 3, "INPUT"],["setPinMode", "digital", 2,"OUTPUT"],["setPinMode", "digital", 3,"PWM"],["setPinMode", "digital", 7,"INPUT"]');
