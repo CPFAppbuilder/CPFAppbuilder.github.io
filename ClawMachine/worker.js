30768aa75e8b89bf0f8733852ba40c16f7ed4ad4
CPFAppbuilder <ziyousong1221@gmail.com>
2017-09-07 16:54:27 +0800

    Add files via upload

--- ClawMachine/worker.js
+++ ClawMachine/worker.js
@@ -1,4 +1,4 @@
 onmessage = function(e) {
 	console.log(e.data);
-	cpf.request('["braccio_movement", 30, 125, 90, 90, 90, 90, 40]');
+	cpf.request('["braccio_movement", 30, 125, 110, 110, 110, 110, 40]');
 }
