01c76923e9c4d449fe61e5e764c63242f0a6d201
CPFAppbuilder <ziyousong1221@gmail.com>
2017-10-17 19:03:30 +0800

    Update bubble.js

--- BubbleGame/js/bubble.js
+++ BubbleGame/js/bubble.js
@@ -161,5 +161,5 @@ animate();
 //cpf設定
 function setup(){
 	if(cpf) 
-		var ret = cpf.setPinMode('["resetPin"],["grove_newChainableLED", 7, 8, 1]'); 
+		cpf.setPinMode('["resetPin"],["grove_newChainableLED", 7, 8, 1]'); 
 }
