a8a6d6cb17cca0349740e7c7182a7626c62d67be
CPFAppbuilder <ziyousong1221@gmail.com>
2017-10-24 17:25:36 +0800

    Update bubble.js

--- BubbleGame/js/bubble.js
+++ BubbleGame/js/bubble.js
@@ -78,8 +78,7 @@ function Circle(x, y, dx, dy, radius, color){
 
 				//LED燈顏色設定
 				if(cpf){
-					//cpf.set("rgb led", this.color); 
-					cpf.request('["grove_setColorRGB", 0, ' + this.color + ']');
+					cpf.set("rgb led", this.color); 
 				}
 
 			}				
