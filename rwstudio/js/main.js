b949de5aada4a888bc0952aba4f201c180ba2ed3
juliah851111 <julia.cpf.test6@gmail.com>
2018-06-02 15:23:59 +0800

    rw

---
+++ rwstudio/js/main.js
@@ -0,0 +1,16 @@
+// $(window).scroll(function(e){
+//     if($(window).scrollTop()<=0)
+//       $(".navbar").addClass("at_top shadow");
+//     else
+//       $(".navbar").removeClass("at_top");
+//   });
+
+
+  
+// $(document).on('click', 'a', function(event){
+//     event.preventDefault();
+//     var target = $(this).attr("href");
+//     $('html, body').animate({
+//         scrollTop: $(target).offset().top
+//     },500);
+// });
