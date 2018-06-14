7245c7e59f4cf95e74a6b157dc652e795a13614b
CPFAppbuilder <ziyousong1221@gmail.com>
2018-06-14 18:08:06 +0800

    Update app.js

--- Fruit/js/app.js
+++ Fruit/js/app.js
@@ -22,19 +22,56 @@ Vue.component('FruitTotal', {
 	template: '#total'
 })
 
+Vue.component('Gallery', {
+	props: ['item', 'index'],
+	template: '#gallery'
+})
+
+
 var app = new Vue({
 	el: '#app',
 	data: {
-		fruit: 'Apple',
-		price: FRUITS['Apple'],
+		fruit: 'Lemon',
+		price: FRUITS['Lemon'],
 		num: 2,
 		weight: 1
 	},
 	methods: {
 		send: function(fruit, weight){
-			stock[fruit] = stock[fruit]-weight
-			aop.aopUpload()
+			// stock[fruit] = stock[fruit]-weight
+			// aop.aopUpload()
+			console.log('aaa')
 		}
 	}
-
 })
+
+var appGet = new Vue({
+	el: '#app_get',
+	data: {
+		//庫存
+		items: {
+			'Apple': '',
+			'Lemon': '',
+			'Orange': '',
+			'Lychee': '',
+			'Grapes': ''
+		}
+	},
+	mounted: function () {
+		let header = {"X-aop-credential": userdata.aopCredentialHeader};
+
+		var self = this;
+		$.ajax({
+			url: url,
+			method: 'GET',
+			headers: header,
+			success: function (data) {
+				self.items = JSON.parse(data);
+			},
+			error: function (error) {
+				console.log(error);
+			}
+		});
+	}
+})
+
